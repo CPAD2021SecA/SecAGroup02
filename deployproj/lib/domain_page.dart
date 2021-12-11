aa0f5881a3fbfbd9d2909db00e5d001d0c8d1fb5
I524742 <pravalika.gandesiri@sap.com>
2021-12-11 20:53:11 +0530

    Changed UI design in domain page

--- deployproj/lib/domain_page.dart
+++ deployproj/lib/domain_page.dart
@@ -5,71 +5,101 @@ import 'package:deployproj/model/domain_model.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:deployproj/home_page.dart';
 
-
 class DomainPage extends StatelessWidget {
   DomainPage({Key key, this.model}) : super(key: key);
   final DomainModel model;
   List<DomainModel> list = [
     DomainModel(
-        name: "Health"),
-    DomainModel(
-        name: "Entertainment"),
-    DomainModel(
-        name: "Education"),
-    DomainModel(
-        name: "Lifestyle"),
+      name: "HEALTH",
+      icon: Icons.favorite,
+    ),
+    DomainModel(name: "ENTERTAINMENT", icon: Icons.audiotrack),
+    DomainModel(name: "EDUCATION", icon: Icons.book),
+    DomainModel(name: "LIFESTYLE", icon: Icons.apps),
   ];
 
-  Widget _category(context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Featured",
-                style: GoogleFonts.inter(fontSize: 14, color: Colors.black87)),
-            SizedBox(
-                width: 49,
-                child: Divider(
-                  color: MColor.green,
-                  thickness: 4,
-                ))
-          ],
-        ),
-      ],
-    );
-  }
+  // Widget _category(context) {
+  //   return Row(
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: <Widget>[
+  //       Column(
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: <Widget>[
+  //           Text("Featured",
+  //               style: GoogleFonts.inter(fontSize: 14, color: Colors.black87)),
+  //           SizedBox(
+  //               width: 49,
+  //               child: Divider(
+  //                 color: MColor.green,
+  //                 thickness: 4,
+  //               ))
+  //         ],
+  //       ),
+  //     ],
+  //   );
+  // }
 
   Widget _cards(context, DomainModel model, int n) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12),
+    return Card(
+      // padding: const EdgeInsets.symmetric(vertical: 12),
+      // backgroundColor: Color(0xff00BCC0),
+      // body: SafeArea(
+      // color: Colors.teal[200],
+      // shape: RoundedRectangleBorder(
+      //   borderRadius: BorderRadius.circular(10.0),
+      // ),
       child: GestureDetector(
         onTap: () {
           Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => HomePage(n : n)));
+              context, MaterialPageRoute(builder: (context) => HomePage(n: n)));
         },
-        child: Row(
-          children: <Widget>[
-            // Expanded(
-            //   child:Text('Demo', style: TextStyle(color: Colors.red),),
-            // ),
-            SizedBox(
-              width: 12,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(model.name,
-                    style: GoogleFonts.inter(
-                        fontWeight: FontWeight.w600, fontSize: 15)),
-                SizedBox(height: 3),
-              ],
-            ),
-            Spacer(),
+        // child: Row(
+        //   children: <Widget>[
+        //     SizedBox(
+        //       width: 100,
+        //     ),
+        //     Column(
+        //       crossAxisAlignment: CrossAxisAlignment.start,
+        //       children: <Widget>[
+        //         Text(model.name,
+        //             textAlign: TextAlign.center,
+        //             style: GoogleFonts.inter(
+        //                 fontWeight: FontWeight.w600, fontSize: 15)),
+        //         SizedBox(height: 25),
+        //       ],
+        //     ),
+        //     Spacer(),
+        //   ],
+        // ),
 
+        // child: Align(
+        //   alignment: Alignment.center,
+        //   child: Text(
+        //     model.name,
+        //     style: TextStyle(
+        //         height: 3,
+        //         color: Colors.black,
+        //         fontWeight: FontWeight.bold,
+        //         fontFamily: 'Montserrat',
+        //         fontSize: 20),
+        //   ),
+        // ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(
+                model.icon,
+                size: 45,
+                color: Colors.blue,
+              ),
+              title: Text(
+                model.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -79,49 +109,49 @@ class DomainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
       body: SafeArea(
         child: SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 12),
-                            child: Icon(MIcons.keyboard_arrow_left),
-                          ),
-                        ),
-                        SizedBox(height: 40),
-                        Text("Find Your",
-                            style: GoogleFonts.inter(
-                                fontSize: 22,
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w300)),
-                        Text("Perfect Mentor",
-                            style: GoogleFonts.inter(
-                                fontSize: 22,
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w500)),
-                        SizedBox(height: 7),
-
-                        SizedBox(height: 25),
-                        _category(context),
-                        SizedBox(height: 20),
-                        _cards(context, list[0], 0),
-                        _cards(context, list[1], 1),
-                        _cards(context, list[2], 2),
-                        _cards(context, list[3], 3),
-                      ],
-                    ))
-              ],
-            )),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Icon(MIcons.keyboard_arrow_left),
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    Text("Find Your",
+                        style: GoogleFonts.inter(
+                            fontSize: 22,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w300)),
+                    Text("Perfect Domain",
+                        style: GoogleFonts.inter(
+                            fontSize: 22,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w500)),
+                    SizedBox(height: 7),
+                    SizedBox(height: 25),
+                    // _category(context),
+                    SizedBox(height: 50),
+                    _cards(context, list[0], 0),
+                    _cards(context, list[1], 1),
+                    _cards(context, list[2], 2),
+                    _cards(context, list[3], 3),
+                  ],
+                ))
+          ],
+        )),
       ),
     );
   }
