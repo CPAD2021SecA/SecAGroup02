cf90b8f4fa06b3d0a7b0a0fbcf336386002bc4ac
Vinayaka Naik <vinayaka.naik@sap.com>
2021-12-17 04:36:23 +0530

    Added first version of working chat

--- deployproj/lib/widget/messages_widget.dart
+++ deployproj/lib/widget/messages_widget.dart
@@ -1,19 +1,27 @@
 // import 'package:deployproj/api/firebase_api.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:deployproj/model/message.dart';
+import 'package:deployproj/service/auth.dart';
+import 'package:deployproj/service/dabase.dart';
+import 'package:deployproj/utils.dart';
 import 'package:deployproj/widget/message_widget.dart';
 import 'package:flutter/material.dart';
 
 class MessagesWidget extends StatelessWidget {
   final String uid;
+  AuthService _auth = AuthService();
 
-  const MessagesWidget({
+   MessagesWidget({
     @required this.uid,
     Key key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => StreamBuilder<List<Message>>(
-        // stream: FirebaseApi.getMessages(idUser),
+  Widget build(BuildContext context) => StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+          .collection('chats/$uid/messages')
+          .orderBy(MessageField.createdAt, descending: true)
+          .snapshots(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
@@ -22,21 +30,27 @@ class MessagesWidget extends StatelessWidget {
               if (snapshot.hasError) {
                 return buildText('Something Went Wrong Try later');
               } else {
-                final messages = snapshot.data;
+                final messages = snapshot.data.docs;
 
-                return messages == null
+                return snapshot.data.docs.length <= 0
                     ? buildText('Say Hi..')
                     : ListView.builder(
                         physics: BouncingScrollPhysics(),
                         reverse: true,
                         itemCount: messages.length,
                         itemBuilder: (context, index) {
-                          final message = messages[index];
+                          
+                          DocumentSnapshot doc = snapshot.data.docs[index];
+                          String idUser = doc['idUser'];
+                          String username = doc['username'];
+                          String message = doc['message'];
+                          DateTime createdAt = Utils.toDateTime(doc['createdAt']);
+                          Message me = Message(idUser: idUser, username: username, message: message, createdAt: createdAt);
 
                           return MessageWidget(
-                            message: message,
-                            isMe: message.idUser ==
-                                "myId", // need to chnge the user dynamically
+                            message: me,
+                            isMe: me.idUser ==
+                                _auth.loggedInUserID(), // need to chnge the user dynamically
                           );
                         },
                       );
